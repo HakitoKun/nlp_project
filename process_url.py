1572fd0defdb14d3464c081e1a214607d7f9a6f1
ttaranto <tom.taranto@yahoo.fr>
2021-12-09 23:02:32 +0100

    Ecriture du csv pour prediction

--- process_url.py
+++ process_url.py
@@ -4,32 +4,38 @@ import os, sys
 import subprocess
 from urllib.parse import urlparse
 
+
 def do_preprocessing(file):
-    bashcommand = "pandoc test/"+file+" +RTS -M6000m -RTS --verbose --toc --trace --mathjax -f latex -t plain --template=template.plain --wrap=none -o test/"+file[:-4]+".txt"
+    bashcommand = "pandoc test/" + file + " +RTS -M6000m -RTS --verbose --toc --trace --mathjax -f latex -t plain --template=template.plain --wrap=none -o test/" + file[
+                                                                                                                                                                    :-4] + ".txt"
+    # Ajouter les balises --verbose et --trace pour avoir un output console
     process = subprocess.Popen(bashcommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     print(error)
     if error is None:
         print("pas d erreurs")
-    return
+    return "test/" + file[:-4] + ".txt"
+
 
 def process_url(pdf_url):
     url = urlparse(pdf_url)
     path = url.path
     elements = path.split("/")
-    dl_url = "/".join([url.scheme+'://www.'+ url.netloc,'e-print',elements[-1][:-4]])
-    return(dl_url)
+    dl_url = "/".join([url.scheme + '://www.' + url.netloc, 'e-print', elements[-1][:-4]])
+    return (dl_url)
+
 
 def create_balise(file):
     pass
 
+
 def main(argv):
     pdf_url = argv[0]
     doc = process_url(pdf_url)
-    #with libreq.urlopen('https://arxiv.org/e-print/2112.04484') as url:
+    # with libreq.urlopen('https://arxiv.org/e-print/2112.04484') as url:
     with libreq.urlopen(doc) as url:
-       r = url.read()
-    #print(r)
+        r = url.read()
+    # print(r)
     with open("test/test.tar", "wb") as f:
         f.write(r)
     tar = tarfile.open("test/test.tar")
@@ -46,9 +52,37 @@ def main(argv):
     for file in tex_files:
         create_balise(file)
 
-
+    list_txt = []
     for file in tex_files:
-        do_preprocessing(file)
+        list_txt.append(do_preprocessing(file))
 
-if __name__=='__main__':
+    for file in list_txt:
+        with open(file, 'r') as f:
+            lines = f.readlines()
+        body_flag = False
+        for line in lines:
+            if not len(line.strip()):
+                continue
+            if line.startswith('BODY'):
+                body_flag = True
+                continue
+            if body_flag:
+                text += line
+            elif line.startswith('ABSTRACT'):
+                continue
+            else:
+                abstract += line
+    print("body :",text)
+    print("abstract : ", abstract)
+    text = text.replace("\"", "'")
+    abstract = abstract.replace("\"", "'")
+    with open("output.txt",'r') as f:
+        f.write("abstract, text")
+        f.write('"' + abstract + '","' + text + '"')
+
+
+
+
+
+if __name__ == '__main__':
     main(sys.argv[1:])
