4f8045f5a7883dc883c7875d82356e5695653ea5
HakitoKun <64664105+HakitoKun@users.noreply.github.com>
2022-01-26 11:06:16 +0100

    fix site

--- site/app.py
+++ site/app.py
@@ -1,4 +1,5 @@
 # CLIENT FRONT END APPLICATION 1 #
+import process_url
 
 from flask import Flask, render_template, url_for, request, redirect
 from datetime import datetime
@@ -9,7 +10,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-        return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/process_url.py', methods=['POST', 'GET'])
+def process():
+    url = request.form["param"]
+    print(url)
+    toto = process_url.main(url)
+    print(toto)
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
